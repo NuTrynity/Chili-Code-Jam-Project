2e530f8ed6a801049f5d746d9f9caf24f46a9d81
ReganUA <162838412+ReganUA@users.noreply.github.com>
2024-12-23 22:57:00 +0200

    Update otcopus_detector.cs

--- Assets/Scripts/PlayerScripts/otcopus_detector.cs
+++ Assets/Scripts/PlayerScripts/otcopus_detector.cs
@@ -4,12 +4,20 @@ public class otcopus_detector : MonoBehaviour
 {
     public GameObject retreat_point;
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    public LayerMask collidableLayers;
+    public Transform shootPoint;
+    private void OnTriggerStay2D(Collider2D other) {
         Debug.Log("Enemy Detected");
         
         if (other.CompareTag("Enemy")) {
             Debug.Log("Enemy Flashed");
-            other.GetComponent<target_manager>().ChangeTarget(retreat_point.transform);
+
+            Vector3 partLighed = other.ClosestPoint(transform.position);
+            RaycastHit2D hit = Physics2D.Raycast(shootPoint.position, (partLighed - shootPoint.position).normalized, 35, collidableLayers);
+            if(hit && hit.collider == other)
+            {
+                other.GetComponent<target_manager>().ChangeTarget(retreat_point.transform);
+            }
         }
     }
 }
