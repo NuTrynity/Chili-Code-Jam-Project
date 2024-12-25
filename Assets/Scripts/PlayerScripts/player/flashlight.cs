c73a477fbc09077692b1b4633776e036e102d9dc
NuTrynity <vargasromeo716@gmail.com>
2024-12-25 10:52:10 +0800

    changed flashlight script

--- Assets/Scripts/PlayerScripts/player/flashlight.cs
+++ Assets/Scripts/PlayerScripts/player/flashlight.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
+    public GeneralMovement gm;
+
     private bool seesEnemy = false;
 
     private void FixedUpdate()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
-        RaycastHit2D light = Physics2D.Raycast(transform.position, direction);
-
-        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+        RaycastHit2D light = Physics2D.Raycast(transform.position, (Vector2)gm.GetMousePosition() - (Vector2)transform.position);
+        transform.rotation = gm.LookAt(gm.GetMousePosition(), 360);
 
         if (light.collider != null)
         {
             seesEnemy = light.collider.CompareTag("Enemy");
         }
-        Vector2 mouseDir = mousePos - transform.position;
+        
+        Vector2 mouseDir = (Vector2)gm.GetMousePosition() - (Vector2)transform.position;
         if (seesEnemy)
         {
             Debug.Log("I see the enemy!");
