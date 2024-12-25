bfe4854e038027b028b3baa47455535d01cc090d
NuTrynity <vargasromeo716@gmail.com>
2024-12-26 06:50:58 +0800

    testing flashlight raycast

--- Assets/Scripts/PlayerScripts/player/flashlight.cs
+++ Assets/Scripts/PlayerScripts/player/flashlight.cs
@@ -3,25 +3,28 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     public GeneralMovement gm;
+    public Transform Enemy;
 
     private bool seesEnemy = false;
 
     private void FixedUpdate()
     {
-        RaycastHit2D light = Physics2D.Raycast(transform.position, (Vector2)gm.GetMousePosition() - (Vector2)transform.position);
-        transform.rotation = gm.LookAt(gm.GetMousePosition(), 360);
+        RaycastHit2D light = Physics2D.Raycast(transform.position, Enemy.position - transform.position);
 
         if (light.collider != null)
         {
-            seesEnemy = light.collider.CompareTag("Enemy");
-        }
-        
-        Vector2 mouseDir = (Vector2)gm.GetMousePosition() - (Vector2)transform.position;
-        if (seesEnemy)
-        {
-            Debug.Log("I see the enemy!");
+            if (light.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Enemy in sight");
+                seesEnemy = true;
+            }
+            else
+            {
+                Debug.Log("Enemy out of sight");
+                seesEnemy = false;
+            }
         }
 
-        Debug.DrawRay(transform.position, mouseDir, Color.red);
+        Debug.DrawRay(transform.position, Enemy.position - transform.position, Color.red);
     }
 }
