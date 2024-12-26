91b5591e93994331d8fa6e739385856557d09ffb
NuTrynity <vargasromeo716@gmail.com>
2024-12-26 16:13:29 +0800

    debug patrol reached

--- Assets/Scripts/Octopus/patrol_ai.cs
+++ Assets/Scripts/Octopus/patrol_ai.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class PatrolAI : MonoBehaviour
+public class patrol_ai : MonoBehaviour
 {
     public float patrol_distance = 5f;
     public float patrol_interval = 5f;
@@ -16,7 +16,16 @@ public class PatrolAI : MonoBehaviour
         StartCoroutine(Patrol());
     }
 
-    // Randomizes a path around the octopus
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.name == "PatrolNode(Clone)")
+        {
+            if (targetManager.destinationSetter != null)
+            {
+                Debug.Log("Patrol node reached");
+            }
+        }
+    }
+
     public Vector2 RandPath()
     {
         float randomDistance = Random.Range(1f, patrol_distance);
@@ -24,15 +33,15 @@ public class PatrolAI : MonoBehaviour
         return new_path;
     }
 
-    private IEnumerator Patrol() {
-        yield return new WaitForSeconds(patrol_interval);
+    private IEnumerator Patrol()
+    {
+        GameObject new_patrol_node = Instantiate(patrol_node);
 
-        GameObject new_patrol_node = patrol_node;
+        yield return new WaitForSeconds(Random.Range(3f, patrol_interval));
 
-        Instantiate(new_patrol_node);
         new_patrol_node.transform.position = RandPath();
         targetManager.destinationSetter.target = new_patrol_node.transform;
 
-        Debug.Log("Patrol Node: " + new_patrol_node.transform.position);
+        StartCoroutine(Patrol());
     }
 }
